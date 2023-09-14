e935379b38e4b2167f5df2f5f62526ccea25538a
vitor-marinho-fjp <vitor.marinho@fjp.mg.gov.br>
2023-09-14 12:22:31 -0300

    Novas Funções

--- Datamaid_FJP.R
+++ Datamaid_FJP.R
@@ -18,26 +18,22 @@ for (pacote in pacotes) {
 }
 
 
-###Carregando Base Saúde IMRS
-# Disponível em: https://docs.google.com/spreadsheets/d/1e8eTy8gwlWFLV8zzEPEB56WR0TkJ5i1u/edit?usp=sharing&ouid=114631608050578367156&rtpof=true&sd=true
+
+
+# Carregar base e gerar relatório -----------------------------------------
+
+# Disponível: https://docs.google.com/spreadsheets/d/1e8eTy8gwlWFLV8zzEPEB56WR0TkJ5i1u/edit?usp=sharing&ouid=114631608050578367156&rtpof=true&sd=true
 
 imrs_saude <- read_excel("IMRS_BASE_SAUDE_2000-2021.xlsx")
 
 
-#  Parte 1: Crítica de Dados ----------------------------------------------
+makeDataReport(imrs_saude, replace=TRUE, output = "html")
 
-
-#1 Distribuição
-p1 <- standardVisual(imrs_saude$S_TXBRUTAMORT, "Taxa bruta de mortalidade")
-
-p2 <-standardVisual(imrs_saude$S_TXMOISQCOR45A59, "Taxa de mortalidade por doenças isquêmicas do coração na população de 45 a 59 anos")
-
-
-p1+p2
+# Adicionando Funções -----------------------------------------------------
 
 
 
-#2------------------------ Trabalhando com funções personalizadas
+#Contar valores zeros
 
 countZeros <- function(v, ...) {
   val <- length(which(v == 0))
@@ -48,16 +44,51 @@ countZeros <- function(v, ...) {
 countZeros(imrs_saude$S_TXMOHOMI15A29)
 
 
+#Função para Identificar Outliers:
+countOutliers <- function(v, threshold = 1.5) {
+  q1 <- quantile(v, 0.25)
+  q3 <- quantile(v, 0.75)
+  iqr <- q3 - q1
+  lower_bound <- q1 - threshold * iqr
+  upper_bound <- q3 + threshold * iqr
+  val <- length(which(v < lower_bound | v > upper_bound))
+  summaryResult(list(feature = "No. outliers", result = val, value = val))
+}
+
+
+# Por grupo
+
+## Crie uma função que calcule estatísticas (média, mediana, etc.) para uma variável numérica,
+# segmentadas por grupos de uma variável categórica. Isso pode ser útil para entender as diferenças entre grupos.
+
+calculateStatsByGroup <- function(data, numeric_var, categorical_var) {
+  stats_by_group <- data %>%
+    group_by({{categorical_var}}) %>%
+    summarise(
+      Mean = mean({{numeric_var}}, na.rm = TRUE),
+      Median = median({{numeric_var}}, na.rm = TRUE),
+      SD = sd({{numeric_var}}, na.rm = TRUE)
+    )
+  summaryResult(list(
+    feature = paste("Summary Statistics by", as_label(categorical_var)),
+    result = stats_by_group,
+    value = NULL
+  ))
+}
+
+
+
 #-----------------------Adicionando novas funções ao report
 
 makeDataReport(imrs_saude, summaries = setSummaries(
-  character = defaultCharacterSummaries(add = "countZeros"),
-  factor = defaultFactorSummaries(add = "countZeros"),
-  labelled = defaultLabelledSummaries(add = "countZeros"),
-  numeric = defaultNumericSummaries(add = "countZeros"),  
-  integer = defaultIntegerSummaries(add = "countZeros"),
-  logical = defaultLogicalSummaries(add = c("countMissing"))
-), replace=TRUE, output = "html")
+  character = defaultCharacterSummaries(add = c("countZeros", "countOutliers")),
+  factor = defaultFactorSummaries(add = c("countZeros", "countOutliers")),
+  labelled = defaultLabelledSummaries(add = c("countZeros", "countOutliers")),
+  numeric = defaultNumericSummaries(add = c("countZeros", "countOutliers")),  
+  integer = defaultIntegerSummaries(add = c("countZeros", "countOutliers")),
+  logical = defaultLogicalSummaries(add = c("countZeros", "countOutliers"))
+), replace = TRUE, output = "html")
+
 
 
 #----------------Recursos Adicionais
